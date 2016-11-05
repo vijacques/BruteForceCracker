be1e59650493d9c415304a588bff936827abdba3
Vinicius Jacques <vjacques@smartcloudinc.com>
2016-11-05 20:46:28 -0200

    testing stuff

--- BruteForceCracker/Program.fs
+++ BruteForceCracker/Program.fs
@@ -1,8 +1,12 @@
 ﻿open System
 open System.Diagnostics
+open System.Threading
 
 [<EntryPoint>]
 let main argv = 
+    // Timer
+    let timer = Stopwatch.StartNew()
+
     // Basic Information
     let program = "C:\Program Files\7-Zip\7z.exe"
     let password = "Hf3"
@@ -23,6 +27,7 @@ let main argv =
     printSeqInfo numSeq
     printSeqInfo lowerABCSeq
     printSeqInfo upperABCSeq
+    printSeqInfo specialSeq
 
     let allSeqs = numSeq |> Seq.append upperABCSeq |> Seq.append lowerABCSeq
 
@@ -51,31 +56,51 @@ let main argv =
 
     printfn "\nProcessor count: %i - Workload per CPU: %i" Environment.ProcessorCount cpuWorkLength
 
-//    let p1 = new Process()
-//    p1.StartInfo.FileName <- program
-//    p1.StartInfo.Arguments <- "a -p" + password + " -y secure.7z BruteForceCracker.exe.config"
-//    p1.StartInfo.RedirectStandardOutput <- true
-//    p1.StartInfo.UseShellExecute <- false
-//    p1.Start() |> ignore
-//    p1.WaitForExit()
-//    printfn "%i %s" p1.ExitCode (p1.StandardOutput.ReadToEnd())
-//    
-//    let p2 = new Process()
-//    p2.StartInfo.FileName <- program
-//    
-//                p2.StartInfo.Arguments <- "x -p" + string n + " -y secure.7z"
-//                p2.StartInfo.RedirectStandardOutput <- true
-//                p2.StartInfo.UseShellExecute <- false
-//                p2.Start() |> ignore
-//                p2.WaitForExit()
-//           
-//                if p2.ExitCode = 0 then
-//                    printfn "%i %s" p2.ExitCode (p2.StandardOutput.ReadToEnd())
-//                    true
-//                else
-//                    printfn "wrong password: %s" n
-//                    false)
-//
+    // Adds password to file
+    let p1 = new Process()
+    p1.StartInfo.FileName <- program
+    p1.StartInfo.Arguments <- "a -p" + password + " -y secure.7z BruteForceCracker.exe.config"
+    p1.StartInfo.RedirectStandardOutput <- true
+    p1.StartInfo.UseShellExecute <- false
+    p1.Start() |> ignore
+    p1.WaitForExit()
+    printfn "%i %s" p1.ExitCode (p1.StandardOutput.ReadToEnd())
+
+    // Cracks password
+    let cancToken = new CancellationTokenSource()
+
+    let bruteCrackAsync s a = async {
+        Seq.iter (fun n ->
+            let p2 = new Process()
+            p2.StartInfo.FileName <- program
     
+            p2.StartInfo.Arguments <- "x -p" + n + " -y secure.7z"
+//            p2.StartInfo.RedirectStandardOutput <- true
+            p2.StartInfo.UseShellExecute <- false
+            p2.Start() |> ignore
+            p2.WaitForExit()
+           
+            if p2.ExitCode = 0 then
+                printfn "%i %s %s" p2.ExitCode (p2.StandardOutput.ReadToEnd()) a
+                timer.Stop()
+                cancToken.Cancel()) s
+//            else
+//                printfn "wrong password: %s" n) s
+
+//            let f = Seq.tryFind (fun n -> n = "Hf3") s
+//        match f with
+//        | Some s ->
+//            printfn "found Hf3!!! - %s" a
+//            timer.Stop()
+//            cancToken.Cancel()
+//        | None -> ()
+    }
+
+    Async.Start (bruteCrackAsync (cpuWorkSeq |> Seq.head) "seq1", cancToken.Token)
+    Async.Start (bruteCrackAsync (cpuWorkSeq |> Seq.skip 1 |> Seq.head) "seq2", cancToken.Token)
+    Async.Start (bruteCrackAsync (cpuWorkSeq |> Seq.skip 2 |> Seq.head) "seq3", cancToken.Token)
+    Async.Start (bruteCrackAsync (cpuWorkSeq |> Seq.skip 3 |> Seq.head) "seq4", cancToken.Token)
+
+    printfn "Total execution time: %f" timer.Elapsed.TotalSeconds
     System.Console.Read() |> ignore
     0
